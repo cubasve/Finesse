c60830491b8b118793aab6453fc266f77a3d9ba8
cubasve <eva.cubasvasquez@gmail.com>
2020-07-26 15:52:55 -0400

    Express back-end now works - deleted a route in routes/api/users.js - WOOOOOO

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 const usersCtrl = require('../../controllers/users');
 
 //Public Routes
 router.post('/signup', usersCtrl.signup);
-router.post('/login', usersCtrl.login);
 
 //Protected Routes
 
