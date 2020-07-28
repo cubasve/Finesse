e9a458ae0da0dfd3e70d94b174f16eb424f32f60
cubasve <eva.cubasvasquez@gmail.com>
2020-07-28 18:40:43 -0400

    Add form submission to passiveIncome

--- src/components/PassiveIncome/PassiveIncome.jsx
+++ src/components/PassiveIncome/PassiveIncome.jsx
@@ -1,30 +1,92 @@
-import React from 'react';
+import React, { Component } from 'react';
+import financialStatementService from '../../utils/financialStatementService';
 
-export default function PassiveIncome(props) {
+const passiveIncomeOptions = ['Real Estate', 'Business', 'Commodities', 'Royalties', 'Other'];
 
-    const passiveIncomeOptions = ['Real Estate', 'Business', 'Commodities', 'Royalties', 'Other'];
-    return (
-        <div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Passive</th>
-                        <th>$</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            <select>
-                                {passiveIncomeOptions.map((option) => (
-                                    <option key={option}>{option}</option>)
-                                )}
-                            </select>
-                        </td>
-                        <td>$<input type="number" min="0" placeholder="" /><button type="submit">+</button></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div >
-    )
+export default class PassiveIncome extends Component {
+    state = {
+        totalPassiveIncome: [],
+        newPassiveIncome: {
+            passiveIncomeType: 'Real Estate',
+            amountEarned: '',
+        },
+        formInvalid: true,
+    }
+    formRef = React.createRef(); //object that provides access to a DOM element - validate form before creating newEarnedIncome
+
+    handleSubmit = async (e) => {
+        // alert('ADD INCOME CLICKED');
+        e.preventDefault();
+        if (!this.formRef.current.checkValidity()) return;
+        try {
+            await financialStatementService.create()
+                .then(
+                    this.setState(state => ({
+                        totalPassiveIncome: [...state.totalPassiveIncome, state.newPassiveIncome],
+                        //add newEarnedIncome onto pre-existing totalEarnedIncome array
+                        newPassiveIncome: { passiveIncomeType: '', amountEarned: '' }
+                        //reset the inputs for better UX
+                    }))
+                )
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    handleChange = e => {
+        const newPassiveIncome = { ...this.state.newPassiveIncome, [e.target.name]: e.target.value }
+        this.setState({ newPassiveIncome: newPassiveIncome, formInvalid: !this.formRef.current.checkValidity() })
+    }
+
+    render() {
+        return (
+            <section>
+                <h4>
+                    <span>Passive</span>
+                    <span>$</span>
+                </h4>
+                {this.state.totalPassiveIncome.map(pi => (
+                    <div key={pi.amountEarned}>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>{pi.passiveIncomeType}</td>
+                                    <td>{pi.amountEarned}</td>
+                                    <td><button value="Update">U</button></td>
+                                    <td><button value="Delete">X</button></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                ))}
+                <form ref={this.formRef} onSubmit={this.handleSubmit}>
+                    <label>
+                        <select
+                            name="passiveIncomeType"
+                            value={this.state.newPassiveIncome.passiveIncomeType}
+                            onChange={this.handleChange}
+                        >
+                            {passiveIncomeOptions.map((option) => (
+                                <option key={option} value={option}>{option}</option>
+                            ))}
+                        </select>
+                    </label>
+                    <label>
+                        <input
+                            name="amountEarned"
+                            value={this.state.newPassiveIncome.amountEarned}
+                            onChange={this.handleChange}
+                            pattern="[1-9]\d{0,}\.?\d{0,2}"
+                            min="0"
+                        />
+                    </label>
+                    <button
+                        className="form-submission"
+                        onClick={this.handleSubmit}
+                        disabled={this.state.formInvalid}
+                    >+</button>
+                </form>
+            </section >
+        )
+    }
 }
--- src/components/PortfolioIncome/PortfolioIncome.jsx
+++ src/components/PortfolioIncome/PortfolioIncome.jsx
@@ -3,36 +3,7 @@ import financialStatementService from '../../utils/financialStatementService';
 
 const portfolioIncomeOptions = ['Stock', 'Bond', 'Index/Mutual Fund', 'GIC', 'REIT', 'Other'];
 
-// export default function PortfolioIncome(props) {
-
-//     const portfolioIncomeOptions = ['Stocks', 'Bonds', 'Index/Mutual Funds', 'GICs', 'REITs', 'Other'];
-//     return (
-//         <div>
-//             <table>
-//                 <thead>
-//                     <tr>
-//                         <th>Portfolio</th>
-//                         <th>$</th>
-//                     </tr>
-//                 </thead>
-//                 <tbody>
-//                     <tr>
-//                         <td>
-//                             <select>
-//                                 {portfolioIncomeOptions.map((option) => (
-//                                     <option key={option}>{option}</option>)
-//                                 )}
-//                             </select>
-//                         </td>
-//                         <td>$<input type="number" min="0" placeholder="Dividends/Interest" /><button type="submit">+</button></td>
-//                     </tr>
-//                 </tbody>
-//             </table>
-//         </div >
-//     )
-// }
-
-export default class EarnedIncome extends Component {
+export default class PortfolioIncome extends Component {
     state = {
         totalPortfolioIncome: [],
         newPortfolioIncome: {
