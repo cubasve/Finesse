6b7ca159bc2f83b3bc624dc1ef686cfc90a1b07c
cubasve <eva.cubasvasquez@gmail.com>
2020-07-28 12:11:43 -0400

    totalEarnedIncome array = req.body -- in console.log

--- controllers/financialStatements.js
+++ controllers/financialStatements.js
@@ -15,27 +15,28 @@ module.exports = {
 //     }
 // }
 
-function create(req, res, next) {
-    const financialStatement = FinancialStatement.create(req.body);
-    console.log(financialStatement)
-    try {
-        console.log(req.body); //console.log working
-        financialStatement.save();
-        res.json({ financialStatement: financialStatement })
-    } catch (err) {
-        res.json({ err });
-        console.error(err);
-        console.log('ERROR: CONTROLLER FN CREATE')
-    }
-}
-
-// async function create(req, res, next) {
+// function create(req, res, next) {
+//     const financialStatement = FinancialStatement.create(req.body);
+//     console.log(financialStatement) //Promise {undefined}
 //     try {
-//         await FinancialStatement.create(req.body);
-//         console.log(req.body); //console.log working
+//         // console.log(req.body); //[ ]
+//         financialStatement.save();
+//         console.log(financialStatement)
+//         res.json({ financialStatement: financialStatement })
 //     } catch (err) {
 //         res.json({ err });
-//         console.error(err)
+//         console.error(err);
 //         console.log('ERROR: CONTROLLER FN CREATE')
 //     }
 // }
+
+function create(req, res, next) {
+    try {
+        FinancialStatement.create(req.body);
+        console.log(req.body); //console.log working
+    } catch (err) {
+        res.json({ err });
+        console.error(err)
+        console.log('ERROR: CONTROLLER FN CREATE')
+    }
+}
