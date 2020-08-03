0cbaf6b967ce50cdfc2efbbef4047b1f5131d020
cubasve <eva.cubasvasquez@gmail.com>
2020-08-03 12:17:06 -0400

    Change to SignUp

--- src/components/SignupForm/SignUpForm.jsx
+++ src/components/SignupForm/SignUpForm.jsx
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from './node_modules/react';
+import { Link } from './node_modules/react-router-dom';
 import userService from '../../utils/userService.js';
 
-export default class SignupForm extends Component {
+export default class SignUpForm extends Component {
     state = {
         name: '',
         email: '',
