213526f547c9a3be5f2dea6bd009e3dd5932b831
cubasve <eva.cubasvasquez@gmail.com>
2020-08-03 13:01:41 -0400

    import React from 'react'

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from './node_modules/react';
+import React, { Component } from 'react';
 import SignUpForm from '../../components/SignupForm/SignUpForm';
 import './SignupPage.css';
 
