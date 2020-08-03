64bf4b37df66487fe9fe0a548993ff04c8ca6875
cubasve <eva.cubasvasquez@gmail.com>
2020-08-03 12:44:32 -0400

    SignupPage.css with lower case 'u'

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from './node_modules/react';
 import SignUpForm from '../../components/SignupForm/SignUpForm';
-import './SignUpPage.css';
+import './SignupPage.css';
 
 export default class SignUpPage extends Component {
     constructor(props) {
