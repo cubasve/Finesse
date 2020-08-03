408a79e7e38b4515d3b6522e47526a233fa68dfa
cubasve <eva.cubasvasquez@gmail.com>
2020-08-03 12:19:27 -0400

    Change to SignUp

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import HomePage from '../HomePage/HomePage';
-import SignupPage from '../SignupPage/SignupPage';
+import SignUpPage from '../SignUpPage/SignUpPage';
 import LoginPage from '../LoginPage/LoginPage';
 import userService from '../../utils/userService';
 import FinancialStatementPage from '../FinancialStatementPage/FinancialStatementPage';
@@ -74,7 +74,7 @@ export default class App extends Component {
           )}>
           </Route>
 
-          <Route exact path="/signup" render={({ history }) => <SignupPage history={history} handleSignupOrLogin={this.handleSignupOrLogin} />}></Route>
+          <Route exact path="/signup" render={({ history }) => <SignUpPage history={history} handleSignupOrLogin={this.handleSignupOrLogin} />}></Route>
 
           <Route exact path="/login" render={({ history }) => <LoginPage history={history} handleSignupOrLogin={this.handleSignupOrLogin} />}></Route>
 
