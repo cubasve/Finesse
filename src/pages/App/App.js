cde1d825617a8c21bf927b70db315a8e0e22dbd8
cubasve <eva.cubasvasquez@gmail.com>
2020-09-18 19:56:39 -0400

    Try again for SignupPage

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import HomePage from '../HomePage/HomePage';
-import SignUpPage from '../SignUpPage/SignUpPage';
+import SignupPage from '../SignUpPage/SignUpPage';
 //Added a capital U to import statement
 
 // ./src/pages/App/App.js
@@ -45,7 +45,7 @@ export default class App extends Component {
           )}>
           </Route>
 
-          <Route exact path="/signup" render={({ history }) => <SignUpPage history={history} handleSignupOrLogin={this.handleSignupOrLogin} />}></Route>
+          <Route exact path="/signup" render={({ history }) => <SignupPage history={history} handleSignupOrLogin={this.handleSignupOrLogin} />}></Route>
 
           <Route exact path="/login" render={({ history }) => <LoginPage history={history} handleSignupOrLogin={this.handleSignupOrLogin} />}></Route>
 
