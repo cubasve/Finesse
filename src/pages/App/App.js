c0b976516189c2c0e66d965e2387495f2be6d18e
cubasve <eva.cubasvasquez@gmail.com>
2020-09-18 19:59:49 -0400

    All U's are capital for SignUpPage

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -45,7 +45,7 @@ export default class App extends Component {
           )}>
           </Route>
 
-          <Route exact path="/signup" render={({ history }) => <SignupPage history={history} handleSignupOrLogin={this.handleSignupOrLogin} />}></Route>
+          <Route exact path="/signup" render={({ history }) => <SignUpPage history={history} handleSignupOrLogin={this.handleSignupOrLogin} />}></Route>
 
           <Route exact path="/login" render={({ history }) => <LoginPage history={history} handleSignupOrLogin={this.handleSignupOrLogin} />}></Route>
 
