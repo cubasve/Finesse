5641b5bd0f346bd50489db303afa0eadb7873d69
cubasve <eva.cubasvasquez@gmail.com>
2021-06-22 23:33:56 -0400

    Add useContext to BadDebt and GoodDebt

--- src/components/BadDebt/BadDebt.jsx
+++ src/components/BadDebt/BadDebt.jsx
@@ -1,113 +1,148 @@
-import React from 'react';
-import { Table, Popover, OverlayTrigger, Button, Form } from 'react-bootstrap';
+import React from "react";
+import AssetLiabilityContext from "../../context/AssetLiabilityContext";
+import { Table, Popover, OverlayTrigger, Button, Form } from "react-bootstrap";
 
-const badDebtOptions = ['Home Mortgage', 'Car Loans', 'Credit Cards', 'School Loans', 'Other'];
+const badDebtOptions = [
+	"Home Mortgage",
+	"Car Loans",
+	"Credit Cards",
+	"School Loans",
+	"Other",
+];
 
 const popover = (
-    <Popover id="popover-basic">
-        <Popover.Title as="h3">Bad Debt</Popover.Title>
-        <Popover.Content>Debt that is used to buy liabilities</Popover.Content>
-    </Popover>
+	<Popover id="popover-basic">
+		<Popover.Title as="h3">Bad Debt</Popover.Title>
+		<Popover.Content>Debt that is used to buy liabilities</Popover.Content>
+	</Popover>
 );
 
 const BadDebtPopover = () => (
-    <OverlayTrigger trigger="click" placement="top" overlay={popover}>
-        <Button variant="success" size="sm">&#8505;</Button>
-    </OverlayTrigger>
+	<OverlayTrigger trigger="click" placement="top" overlay={popover}>
+		<Button variant="success" size="sm">
+			&#8505;
+		</Button>
+	</OverlayTrigger>
 );
 
 function calculateBadDebtPercentage(totalLiabilities, totalBadDebt) {
-    if (!totalLiabilities || !totalBadDebt) return 0;
-    const percentage = (totalBadDebt / totalLiabilities) * 100;
-    if (Number.isInteger(percentage)) return percentage;
-    const result = percentage.toFixed(1);
-    return result;
+	if (!totalLiabilities || !totalBadDebt) return 0;
+	const percentage = (totalBadDebt / totalLiabilities) * 100;
+	if (Number.isInteger(percentage)) return percentage;
+	const result = percentage.toFixed(1);
+	return result;
 }
 
 function calculateBadDebt(totalBadDebtNumber) {
-    if (!totalBadDebtNumber) return 0;
-    if (Number.isInteger(totalBadDebtNumber)) return totalBadDebtNumber;
-    return totalBadDebtNumber.toFixed(2);
+	if (!totalBadDebtNumber) return 0;
+	if (Number.isInteger(totalBadDebtNumber)) return totalBadDebtNumber;
+	return totalBadDebtNumber.toFixed(2);
 }
 
-export default function BadDebt({ totalBadDebt, handleBadDebtDelete, badDebtFormRef, handleBadDebtSubmit, newBadDebt, handleBadDebtChange, badDebtFormInvalid, totalLiabilities }) {
+export default function BadDebt() {
+	const {
+		totalBadDebt,
+		handleBadDebtDelete,
+		badDebtFormRef,
+		handleBadDebtSubmit,
+		newBadDebt,
+		handleBadDebtChange,
+		badDebtFormInvalid,
+		totalLiabilities,
+	} = useContext(AssetLiabilityContext);
 
-    const totalLiabilityNumber = totalLiabilities.map(elem => elem.amount).reduce((acc, num) => acc + num, 0);
-    const totalBadDebtNumber = totalBadDebt.map(elem => elem.amount).reduce((acc, num) => acc + num, 0);
+	const totalLiabilityNumber = totalLiabilities
+		.map((elem) => elem.amount)
+		.reduce((acc, num) => acc + num, 0);
 
-    return (
-        <>
-            <h5>
-                <span className="left percentage">{calculateBadDebtPercentage(totalLiabilityNumber, totalBadDebtNumber)}%</span>
-                <span>Bad Debt <BadDebtPopover /></span>
-                <span className="right">${calculateBadDebt(totalBadDebtNumber)}</span>
-            </h5>
-            {totalBadDebt.map(bd => (
-                <div key={bd.amount}>
-                    <Table borderless hover size="sm">
-                        <tbody>
-                            <tr>
-                                <td className="left">
-                                    <Button
-                                        name={bd.amount}
-                                        value={bd._id}
-                                        onClick={handleBadDebtDelete}
-                                        variant="danger"
-                                        size="sm"
-                                        className="delete">X</Button>
-                                    {bd.type}
-                                </td>
-                                <td className="right">{bd.amount}</td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                </div>
-            ))
-            }
-            <Form ref={badDebtFormRef} onSubmit={handleBadDebtSubmit}>
-                <Form.Row>
-                    <Form.Group>
-                        <Form.Control
-                            name="type"
-                            value={newBadDebt.type}
-                            onChange={handleBadDebtChange}
-                            as="select"
-                            size="sm"
-                            className="select"
-                        >
-                            {badDebtOptions.map((option) => (
-                                <option key={option} value={option}>{option}</option>
-                            ))}
-                        </Form.Control>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Control
-                            name="amount"
-                            value={newBadDebt.amount}
-                            onChange={handleBadDebtChange}
-                            required
-                            pattern="[1-9]\d{0,}\.?\d{0,2}"
-                            placeholder="Debt Value"
-                            autoComplete="off"
-                            size="sm"
-                        />
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Control
-                            type="hidden"
-                            name="category"
-                            value={newBadDebt.category}
-                            onChange={handleBadDebtChange}
-                        />
-                        <Button
-                            className="form-submission"
-                            onClick={handleBadDebtSubmit}
-                            disabled={badDebtFormInvalid}
-                            size="sm"
-                        >ADD</Button>
-                    </Form.Group>
-                </Form.Row>
-            </Form>
-        </>
-    )
+	const totalBadDebtNumber = totalBadDebt
+		.map((elem) => elem.amount)
+		.reduce((acc, num) => acc + num, 0);
+
+	return (
+		<>
+			<h5>
+				<span className="left percentage">
+					{calculateBadDebtPercentage(totalLiabilityNumber, totalBadDebtNumber)}
+					%
+				</span>
+				<span>
+					Bad Debt <BadDebtPopover />
+				</span>
+				<span className="right">${calculateBadDebt(totalBadDebtNumber)}</span>
+			</h5>
+			{totalBadDebt.map((bd) => (
+				<div key={bd.amount}>
+					<Table borderless hover size="sm">
+						<tbody>
+							<tr>
+								<td className="left">
+									<Button
+										name={bd.amount}
+										value={bd._id}
+										onClick={handleBadDebtDelete}
+										variant="danger"
+										size="sm"
+										className="delete"
+									>
+										X
+									</Button>
+									{bd.type}
+								</td>
+								<td className="right">{bd.amount}</td>
+							</tr>
+						</tbody>
+					</Table>
+				</div>
+			))}
+			<Form ref={badDebtFormRef} onSubmit={handleBadDebtSubmit}>
+				<Form.Row>
+					<Form.Group>
+						<Form.Control
+							name="type"
+							value={newBadDebt.type}
+							onChange={handleBadDebtChange}
+							as="select"
+							size="sm"
+							className="select"
+						>
+							{badDebtOptions.map((option) => (
+								<option key={option} value={option}>
+									{option}
+								</option>
+							))}
+						</Form.Control>
+					</Form.Group>
+					<Form.Group>
+						<Form.Control
+							name="amount"
+							value={newBadDebt.amount}
+							onChange={handleBadDebtChange}
+							required
+							pattern="[1-9]\d{0,}\.?\d{0,2}"
+							placeholder="Debt Value"
+							autoComplete="off"
+							size="sm"
+						/>
+					</Form.Group>
+					<Form.Group>
+						<Form.Control
+							type="hidden"
+							name="category"
+							value={newBadDebt.category}
+							onChange={handleBadDebtChange}
+						/>
+						<Button
+							className="form-submission"
+							onClick={handleBadDebtSubmit}
+							disabled={badDebtFormInvalid}
+							size="sm"
+						>
+							ADD
+						</Button>
+					</Form.Group>
+				</Form.Row>
+			</Form>
+		</>
+	);
 }
--- src/components/GoodDebt/GoodDebt.jsx
+++ src/components/GoodDebt/GoodDebt.jsx
@@ -1,112 +1,151 @@
-import React from 'react';
-import { Table, Popover, OverlayTrigger, Button, Form } from 'react-bootstrap';
+import React from "react";
+import AssetLiabilityContext from "../../context/AssetLiabilityContext";
+import { Table, Popover, OverlayTrigger, Button, Form } from "react-bootstrap";
 
-const goodDebtOptions = ['Real Estate', 'Business', 'Paper', 'Commodities', 'Other'];
+const goodDebtOptions = [
+	"Real Estate",
+	"Business",
+	"Paper",
+	"Commodities",
+	"Other",
+];
 
 const popover = (
-    <Popover id="popover-basic">
-        <Popover.Title as="h3">Good Debt</Popover.Title>
-        <Popover.Content>Debt that is used to buy assets</Popover.Content>
-    </Popover>
+	<Popover id="popover-basic">
+		<Popover.Title as="h3">Good Debt</Popover.Title>
+		<Popover.Content>Debt that is used to buy assets</Popover.Content>
+	</Popover>
 );
 
 const GoodDebtPopover = () => (
-    <OverlayTrigger trigger="click" placement="top" overlay={popover}>
-        <Button variant="success" size="sm">&#8505;</Button>
-    </OverlayTrigger>
+	<OverlayTrigger trigger="click" placement="top" overlay={popover}>
+		<Button variant="success" size="sm">
+			&#8505;
+		</Button>
+	</OverlayTrigger>
 );
 
 function calculateGoodDebtPercentage(totalLiabilities, totalGoodDebt) {
-    if (!totalLiabilities || !totalGoodDebt) return 0;
-    const percentage = (totalGoodDebt / totalLiabilities) * 100;
-    if (Number.isInteger(percentage)) return percentage;
-    const result = percentage.toFixed(1);
-    return result;
+	if (!totalLiabilities || !totalGoodDebt) return 0;
+	const percentage = (totalGoodDebt / totalLiabilities) * 100;
+	if (Number.isInteger(percentage)) return percentage;
+	const result = percentage.toFixed(1);
+	return result;
 }
 
 function calculateGoodDebt(totalGoodDebtNumber) {
-    if (!totalGoodDebtNumber) return 0;
-    if (Number.isInteger(totalGoodDebtNumber)) return totalGoodDebtNumber;
-    return totalGoodDebtNumber.toFixed(2);
+	if (!totalGoodDebtNumber) return 0;
+	if (Number.isInteger(totalGoodDebtNumber)) return totalGoodDebtNumber;
+	return totalGoodDebtNumber.toFixed(2);
 }
 
-export default function GoodDebt({ totalGoodDebt, handleGoodDebtDelete, goodDebtFormRef, handleGoodDebtSubmit, newGoodDebt, handleGoodDebtChange, goodDebtFormInvalid, totalLiabilities }) {
+export default function GoodDebt() {
+	const {
+		totalGoodDebt,
+		handleGoodDebtDelete,
+		goodDebtFormRef,
+		handleGoodDebtSubmit,
+		newGoodDebt,
+		handleGoodDebtChange,
+		goodDebtFormInvalid,
+		totalLiabilities,
+	} = useContext(AssetLiabilityContext);
 
-    const totalLiabilityNumber = totalLiabilities.map(elem => elem.amount).reduce((acc, num) => acc + num, 0);
-    const totalGoodDebtNumber = totalGoodDebt.map(elem => elem.amount).reduce((acc, num) => acc + num, 0);
+	const totalLiabilityNumber = totalLiabilities
+		.map((elem) => elem.amount)
+		.reduce((acc, num) => acc + num, 0);
 
-    return (
-        <>
-            <h5>
-                <span className="left percentage">{calculateGoodDebtPercentage(totalLiabilityNumber, totalGoodDebtNumber)}%</span>
-                <span>Good Debt <GoodDebtPopover /></span>
-                <span className="right">${calculateGoodDebt(totalGoodDebtNumber)}</span>
-            </h5>
-            {totalGoodDebt.map(gd => (
-                <div key={gd.amount}>
-                    <Table borderless hover size="sm">
-                        <tbody>
-                            <tr>
-                                <td className="left">
-                                    <Button
-                                        name={gd.amount}
-                                        value={gd._id}
-                                        onClick={handleGoodDebtDelete}
-                                        variant="danger"
-                                        size="sm"
-                                        className="delete">X</Button>
-                                    {gd.type}
-                                </td>
-                                <td className="right">{gd.amount}</td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                </div>
-            ))}
-            <Form ref={goodDebtFormRef} onSubmit={handleGoodDebtSubmit}>
-                <Form.Row>
-                    <Form.Group>
-                        <Form.Control
-                            name="type"
-                            value={newGoodDebt.type}
-                            onChange={handleGoodDebtChange}
-                            as="select"
-                            size="sm"
-                            className="select"
-                        >
-                            {goodDebtOptions.map((option) => (
-                                <option key={option} value={option}>{option}</option>
-                            ))}
-                        </Form.Control>
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Control
-                            name="amount"
-                            value={newGoodDebt.amount}
-                            onChange={handleGoodDebtChange}
-                            required
-                            pattern="[1-9]\d{0,}\.?\d{0,2}"
-                            placeholder="Debt Value"
-                            autoComplete="off"
-                            size="sm"
-                        />
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Control
-                            type="hidden"
-                            name="category"
-                            value={newGoodDebt.category}
-                            onChange={handleGoodDebtChange}
-                        />
-                        <Button
-                            className="form-submission"
-                            onClick={handleGoodDebtSubmit}
-                            disabled={goodDebtFormInvalid}
-                            size="sm"
-                        >ADD</Button>
-                    </Form.Group>
-                </Form.Row>
-            </Form>
-        </>
-    )
+	const totalGoodDebtNumber = totalGoodDebt
+		.map((elem) => elem.amount)
+		.reduce((acc, num) => acc + num, 0);
+
+	return (
+		<>
+			<h5>
+				<span className="left percentage">
+					{calculateGoodDebtPercentage(
+						totalLiabilityNumber,
+						totalGoodDebtNumber
+					)}
+					%
+				</span>
+				<span>
+					Good Debt <GoodDebtPopover />
+				</span>
+				<span className="right">${calculateGoodDebt(totalGoodDebtNumber)}</span>
+			</h5>
+			{totalGoodDebt.map((gd) => (
+				<div key={gd.amount}>
+					<Table borderless hover size="sm">
+						<tbody>
+							<tr>
+								<td className="left">
+									<Button
+										name={gd.amount}
+										value={gd._id}
+										onClick={handleGoodDebtDelete}
+										variant="danger"
+										size="sm"
+										className="delete"
+									>
+										X
+									</Button>
+									{gd.type}
+								</td>
+								<td className="right">{gd.amount}</td>
+							</tr>
+						</tbody>
+					</Table>
+				</div>
+			))}
+			<Form ref={goodDebtFormRef} onSubmit={handleGoodDebtSubmit}>
+				<Form.Row>
+					<Form.Group>
+						<Form.Control
+							name="type"
+							value={newGoodDebt.type}
+							onChange={handleGoodDebtChange}
+							as="select"
+							size="sm"
+							className="select"
+						>
+							{goodDebtOptions.map((option) => (
+								<option key={option} value={option}>
+									{option}
+								</option>
+							))}
+						</Form.Control>
+					</Form.Group>
+					<Form.Group>
+						<Form.Control
+							name="amount"
+							value={newGoodDebt.amount}
+							onChange={handleGoodDebtChange}
+							required
+							pattern="[1-9]\d{0,}\.?\d{0,2}"
+							placeholder="Debt Value"
+							autoComplete="off"
+							size="sm"
+						/>
+					</Form.Group>
+					<Form.Group>
+						<Form.Control
+							type="hidden"
+							name="category"
+							value={newGoodDebt.category}
+							onChange={handleGoodDebtChange}
+						/>
+						<Button
+							className="form-submission"
+							onClick={handleGoodDebtSubmit}
+							disabled={goodDebtFormInvalid}
+							size="sm"
+						>
+							ADD
+						</Button>
+					</Form.Group>
+				</Form.Row>
+			</Form>
+		</>
+	);
 }
